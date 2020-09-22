bd415438a98a2ca85e1944320441c69673fd10ea
HEXcube <hexcubed@gmail.com>
2020-09-22 11:41:24 +0530

    Add function to remove grid item

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ function spawnGridItem() {
   // https://stackoverflow.com/questions/28175841/jquery-how-clone-bootstrap-row#28175954
   $(".sample-components .col").clone().hide().insertBefore(".spawn-button-column").fadeIn("slow")
 }
+
+function removeGridItem(thisObj) {
+  /*
+    https://stackoverflow.com/questions/5333426/how-to-find-a-parent-with-a-known-class-in-jquery/5333443#5333443
+  */
+  $(thisObj).closest('.grid-item').remove()
+}
