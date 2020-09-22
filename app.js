aee797ad9dafddda779b64b87448ba567492c36c
HEXcube <hexcubed@gmail.com>
2020-09-22 11:24:53 +0530

    Add html for remove button
    - Add unicode symbols as remove and add icons

--- app.js
+++ app.js
@@ -4,5 +4,6 @@
  */
 
 function spawnGridItem() {
+  // https://stackoverflow.com/questions/28175841/jquery-how-clone-bootstrap-row#28175954
   $(".sample-components .col").clone().hide().insertBefore(".spawn-button-column").fadeIn("slow")
 }
